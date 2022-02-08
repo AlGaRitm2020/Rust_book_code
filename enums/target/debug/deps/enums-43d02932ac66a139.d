09650962513cb5103551e386f9e56954e7e23427
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-02-08 20:07:29 +0400

    created enum block

---
+++ enums/target/debug/deps/enums-43d02932ac66a139.d
@@ -0,0 +1,5 @@
+/home/algaritm/Projects/rust_project/enums/target/debug/deps/enums-43d02932ac66a139: src/main.rs
+
+/home/algaritm/Projects/rust_project/enums/target/debug/deps/enums-43d02932ac66a139.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/home/algaritm/Projects/rust_project/enums/target/debug/enums: /home/algaritm/Projects/rust_project/enums/src/main.rs
